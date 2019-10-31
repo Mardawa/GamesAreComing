7800ef5c13a555a6a70c98a04f43e285dc81ba01
Mardawa <48473818+Mardawa@users.noreply.github.com>
2019-10-31 11:11:21 +0100

    'store' is not defined  no-undef fixed

--- client/src/store.js
+++ client/src/store.js
@@ -1,27 +1,24 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-if (process.env.NODE_ENV === "production") {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware))
-  );
-} else {
-  const store = createStore(
+const store = process.env.NODE_ENV !== 'production'? (createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-}
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+)):(createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware)
+        )
+))
 
 export default store;
